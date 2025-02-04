7b4f6fc89da8132d9932d5195a0731b88b5ae7a7
homailahmed <homailsunflowerind@gmail.com>
2025-02-04 10:35:19 -0800

    Add files via upload
    Certification 1

---
+++ certification.py
@@ -0,0 +1,10 @@
+my_percentage: int = int(input("enter your marks: "))
+msrks = 86
+if marks >= 90:
+   print("you got A+ grade")
+elif marks >=80 and msrks <=90:
+   print("you got B+ grade")
+elif marks >=80 and msrks <=90:
+   grade = "C"
+else:
+   print("your grade is ") 
